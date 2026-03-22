33a79b7b1d08b28a0707e898f85774ac63c1966c
KPH <whereiskurt@gmail.com>
2026-03-22 00:45:13 -0400

    test(03-00): add sidecar test stubs (dns-proxy, http-proxy, audit-log)
    - dns_proxy_test.go: 5 failing stubs in package dnsproxy_test
    - http_proxy_test.go: 5 failing stubs in package httpproxy_test
    - audit_log_test.go: 5 failing stubs in package auditlog_test
    - all compile and fail with t.Fatal("not implemented")

---
+++ sidecars/audit-log/audit_log_test.go
@@ -0,0 +1,9 @@
+package auditlog_test
+
+import "testing"
+
+func TestAuditLogFormat_ShellCommand(t *testing.T) { t.Fatal("not implemented") }
+func TestAuditLogFormat_NetworkEvent(t *testing.T) { t.Fatal("not implemented") }
+func TestAuditLogDest_Stdout(t *testing.T)         { t.Fatal("not implemented") }
+func TestAuditLogDest_CloudWatch(t *testing.T)     { t.Fatal("not implemented") }
+func TestAuditLogFormat_InvalidJSON(t *testing.T)  { t.Fatal("not implemented") }
---
+++ sidecars/http-proxy/http_proxy_test.go
@@ -0,0 +1,9 @@
+package httpproxy_test
+
+import "testing"
+
+func TestHTTPProxy_AllowedHost(t *testing.T)         { t.Fatal("not implemented") }
+func TestHTTPProxy_BlockedHost(t *testing.T)         { t.Fatal("not implemented") }
+func TestHTTPProxy_AllowedWithPort(t *testing.T)     { t.Fatal("not implemented") }
+func TestHTTPProxy_TraceparentInjected(t *testing.T) { t.Fatal("not implemented") }
+func TestIsHostAllowed(t *testing.T)                 { t.Fatal("not implemented") }
